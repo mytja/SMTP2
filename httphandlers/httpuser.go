ff70f1cfcff171b8a903d5958a9cb2a77e968ff8
mytja <mytja@protonmail.com>
2021-12-10 18:56:05 +0100

    Improve SQL calls in httpuser.go

--- httphandlers/httpuser.go
+++ httphandlers/httpuser.go
@@ -2,7 +2,6 @@ package httphandlers
 
 import (
 	"github.com/mytja/SMTP2/helpers"
-	"github.com/mytja/SMTP2/objects"
 	crypto2 "github.com/mytja/SMTP2/security/crypto"
 	"net/http"
 )
@@ -11,9 +10,7 @@ func (server *httpImpl) Login(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("Email")
 	pass := r.FormValue("Pass")
 	// Check if password is valid
-	var user objects.User
-	// TODO: Tole je lahko bolje
-	err := server.db.GetDB().Get(&user, "SELECT * FROM users WHERE email=$1", email)
+	user, err := server.db.GetUserByEmail(email)
 	hashCorrect := crypto2.CheckHash(pass, user.Password)
 	if !hashCorrect {
 		WriteJSON(w, Response{Data: "Hashes don't match...", Success: false}, http.StatusForbidden)
@@ -47,10 +44,8 @@ func (server *httpImpl) NewUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Check if user is already in DB
-	var user objects.User
 	var userCreated = true
-	// TODO: Vem da je tole lahko bolje
-	err = server.db.GetDB().Get(&user, "SELECT * FROM users WHERE email=$1", email)
+	_, err = server.db.GetUserByEmail(email)
 	if err != nil {
 		if err.Error() == "sql: no rows in result set" {
 			userCreated = false
