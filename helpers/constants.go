bb583e9f95f1a029bc7e3e83615fbf14b3e28b6d
mytja <mytja@protonmail.com>
2022-03-21 16:55:57 +0100

    Random signing key - Resolves #21

--- helpers/constants.go
+++ helpers/constants.go
@@ -1,5 +1,10 @@
 package helpers
 
+import (
+	"github.com/dchest/uniuri"
+	"os"
+)
+
 func GetSigningKey() []byte {
 	if os.Getenv("SMTP2_HOST_URL") != "" {
 		return []byte(uniuri.NewLen(100))
@@ -7,7 +12,7 @@ func GetSigningKey() []byte {
 	return []byte("46ad2cb520028e1f5e2eab8d860a547353ddbabdb6affb923c075c92518c7e02")
 }
 
-var JwtSigningKey []byte = StringToBytearray("46ad2cb520028e1f5e2eab8d860a547353ddbabdb6affb923c075c92518c7e02")
+var JwtSigningKey = GetSigningKey()
 
 const JwtIssuer = "SMTP2AuthCA"
 
