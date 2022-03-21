cd678d087c6d65529d3a8d521d5a55a70f52ab4b
Mitja <mytja@protonmail.com>
2022-03-21 16:35:54 +0100

    Add random signature generation [skip ci]

--- helpers/constants.go
+++ helpers/constants.go
@@ -1,5 +1,12 @@
 package helpers
 
+func GetSigningKey() []byte {
+	if os.Getenv("SMTP2_HOST_URL") != "" {
+		return []byte(uniuri.NewLen(100))
+	}
+	return []byte("46ad2cb520028e1f5e2eab8d860a547353ddbabdb6affb923c075c92518c7e02")
+}
+
 var JwtSigningKey []byte = StringToBytearray("46ad2cb520028e1f5e2eab8d860a547353ddbabdb6affb923c075c92518c7e02")
 
 const JwtIssuer = "SMTP2AuthCA"
